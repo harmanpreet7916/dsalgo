c68218186273e37dc24387f46530bc9fef30aa80
happyoye <116631175+happyoye@users.noreply.github.com>
2022-10-25 14:58:30 +0530

    adding findfactorial
    program to find factorial of a number

---
+++ searching/FindFactorial.c
@@ -0,0 +1,23 @@
+//Calculating factorial of given number in C language usingif Recurssion
+#include<bits/stdc++.h>    // This is the main header
+#include<stdio.h>
+int fun(int n);
+int main()
+{
+	int n,i,a,fact=1;
+	printf("Enter a Number :\n);
+	scanf("%d",&n);
+	p=fun(n);
+	printf("%d",p);
+	return 0;
+}
+
+int fun(a){
+	if(a>=1)
+	{
+		return(a*fun(a-1));
+	}
+	else{
+		return 1;
+	}
+}
